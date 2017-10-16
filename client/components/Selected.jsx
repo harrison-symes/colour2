0c04b532b19d4b37e1a4a82947a08e640b4bef42
harrison-symes <symeshjb@gmail.com>
2017-10-16 16:29:10 +1300

    a little cleaner

--- client/components/Selected.jsx
+++ client/components/Selected.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Selected (props) {
   const {colours, unSelect} = props
-  return <div className="columns is-multiline">
+  return <div className="section columns is-multiline">
     {colours.map((c, i ) => <div  key={i} className="column is-2" style={{backgroundColor: c.colour, height: '15vh'}}>
         <h1 className="subtitle is-6">{c.colour}</h1>
         <button className="delete" onClick={() => unSelect(c)}/>
